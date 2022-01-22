e050aea29f3d37c424b023a336b891e04394ca9a
Edoardo Marchionni <edoardo.marchionni@gmail.com>
2022-01-22 19:53:27 +0100

    new initial tables

--- MCMC/Initialization tables/Old/Functions_old.R
+++ MCMC/Initialization tables/Old/Functions_old.R
@@ -65,35 +65,15 @@ col_filling <- function(TAB, ctot, rtot, j){
       
       if(rtot[3] + rtot[4] + rtot[6] >= ctot[2]){
         
-        
         new_rtot <- c(0,0,rtot[3],rtot[4],0,rtot[6],0)
+        temp <- sort(c(rtot[3],rtot[4],rtot[6]))
         
-        if(sum(new_rtot)>0){
-          
-          i <- which(new_rtot == max(new_rtot))
-          TAB[i,2] <- min(rtot[i],ctot[2]); ctot[2] <- ctot[2] - TAB[i,2]
-          #browser()
-          new_rtot[i] <- 0
-          
-          if(sum(new_rtot)>0){
-            
-            i <- which(new_rtot == max(new_rtot))
-            TAB[i,2] <- min(rtot[i],ctot[2]); ctot[2] <- ctot[2] - TAB[i,2]
-            new_rtot[i] <- 0
-            
-            if(sum(new_rtot)>0){
-              
-              i <- which(new_rtot == max(new_rtot))
-              TAB[i,2] <- min(rtot[i],ctot[2]); ctot[2] <- ctot[2] - TAB[i,2]
-              
-            }
-            
-          }
-          
-        }
-
-        
-        
+        i <- which(new_rtot == temp[1])
+        TAB[i,2] <- min(rtot[i],ctot[2]); ctot[2] <- ctot[2] - TAB[i,2]
+        i <- which(new_rtot == temp[2])
+        TAB[i,2] <- min(rtot[i],ctot[2]); ctot[2] <- ctot[2] - TAB[i,2]
+        i <- which(new_rtot == temp[3])
+        TAB[i,2] <- min(rtot[i],ctot[2]); ctot[2] <- ctot[2] - TAB[i,2]
         
       } else {
         return(FALSE)
@@ -176,32 +156,16 @@ col_filling <- function(TAB, ctot, rtot, j){
       if(rtot[3] + rtot[4] + rtot[5] >= ctot[4]){
         
         TAB[4,4] <- 0
-        new_rtot <- c(0,0,rtot[3],rtot[4],rtot[5],0,0)
         
-        if(sum(new_rtot)>0){
-          
-          i <- which(new_rtot == max(new_rtot))
-          TAB[i,4] <- min(new_rtot[i],ctot[4]); ctot[4] <- ctot[4] - TAB[i,4]
-          #browser()
-          new_rtot[i] <- 0
-          
-          if(sum(new_rtot)>0){
-            
-            i <- which(new_rtot == max(new_rtot))
-            TAB[i,4] <- min(new_rtot[i],ctot[4]); ctot[4] <- ctot[4] - TAB[i,4]
-            new_rtot[i] <- 0
-            
-            if(sum(new_rtot)>0){
-              
-              i <- which(rtot == max(new_rtot))
-              TAB[i,4] <- min(new_rtot[i],ctot[4]); ctot[4] <- ctot[4] - TAB[i,4]
-              
-            }
-            
-          }
-          
-        }
-
+        temp <- sort(c(rtot[3],rtot[4],rtot[5]))
+        
+        i <- which(rtot == temp[1])
+        TAB[i,4] <- min(rtot[i],ctot[4]); ctot[4] <- ctot[4] - TAB[i,4]
+        i <- which(rtot == temp[2])
+        TAB[i,4] <- min(rtot[i],ctot[4]); ctot[4] <- ctot[4] - TAB[i,4]
+        i <- which(rtot == temp[3])
+        TAB[i,4] <- min(rtot[i],ctot[4]); ctot[4] <- ctot[4] - TAB[i,4]
+    
         TAB[4,4] <- TAB[4,4] + d
         
       } else {
